9042f6eef19cd0898db508d1afd6e647e5d903e0
Radek Benes <radekbenes@centrum.cz>
2016-07-20 12:50:27 +0200

    20-07-16

--- app/pages/add-workout/add-workout.ts
+++ app/pages/add-workout/add-workout.ts
@@ -1,10 +1,34 @@
 import {Component} from '@angular/core';
 import {NavController} from 'ionic-angular';
+import {WorkoutService} from '../../services/workout.service';
 
 @Component({
-  templateUrl: 'build/pages/add-workout/add-workout.html'
+  templateUrl: 'build/pages/add-workout/add-workout.html',
+  providers: [WorkoutService]
 })
 export class AddWorkoutPage {
-  constructor(private navController: NavController) {
+  static get parameters() {
+    return [[WorkoutService]]
+  }
+  
+  constructor(private workoutService: WorkoutService, private navController: NavController, private title: string, private note: string, private type: string) {
+    this.workoutService = workoutService;
+    this.title;
+    this.note;
+    this.type;
+  }
+
+  onSubmit() {
+    var workout = {
+      title: this.title,
+      note: this.note,
+      type: this.type
+    }
+
+    this.workoutService.addWorkout(workout).subscribe(data => {
+      this.result = data
+    },
+    err => console.log(err),
+    () => console.log('Workout added'));
   }
 }
